5630f636c08ab7a93ba51defaf0b4316da6bb25e
BizLeapTechnology <bizleaptechnologyassignment@gmail.com>
2018-02-13 14:17:01 +0630

    WarWarMyo.java

---
+++ WarWarMyo.java
@@ -0,0 +1,13 @@
+public class WarWarMyo {
+    public static void main(String[] args) {
+        String myname="War War Myo";
+        String newname=new StringBuilder(myname).reverse().toString();
+        System.out.println(newname);
+    }
+}
+
+/*
+- Fail Test
+- Reverse everything
+- Does not follow naming convention
+ */
