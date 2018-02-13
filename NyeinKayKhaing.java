f9b8e51e574f5f47d81f8aa76eac13fbceb55d25
BizLeapTechnology <bizleaptechnologyassignment@gmail.com>
2018-02-13 15:08:59 +0630

    NyeinKayKhaing.java

---
+++ NyeinKayKhaing.java
@@ -0,0 +1,14 @@
+public class NyeinKayKhaing {
+    public static void main(String[] args) {
+        String input = "First Middle Last";
+        StringBuilder input1 = new StringBuilder();
+        input1.append(input);
+        input1 = input1.reverse();
+        System.out.println(input1);
+    }
+}
+
+/*
+- Fail Testing
+- Reverse Everything
+ */
