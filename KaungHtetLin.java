3220e2f3e03f16e1655a3bd6fb00dad8ff5f3d9e
BizLeapTechnology <bizleaptechnologyassignment@gmail.com>
2018-02-13 11:58:46 +0630

    KaungHtetLin.java

---
+++ KaungHtetLin.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class KaungHtetLin {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        ArrayList al =new ArrayList();
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        while(st.hasMoreTokens())
+            al.add(st.nextToken().toString());
+        Collections.reverse(al);
+        System.out.println(al);
+    }
+}
+
+/*
+- Pass Testing
+- Follow naming convention
+- 10 of 10
+ */
