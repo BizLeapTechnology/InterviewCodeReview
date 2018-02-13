51a3920aa415ac85695ab739fc39dd1623b3a841
BizLeapTechnology <bizleaptechnologyassignment@gmail.com>
2018-02-13 15:36:58 +0630

    HtetHtetSan.java

---
+++ HtetHtetSan.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class HtetHtetSan {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Please insert a sentence you want to reverse : ");
+        String sentence=scanner.nextLine();
+        String result="";
+        String[] words=sentence.split(" ");
+        for (int i=words.length-1;i>=0;i--){
+            result+=words[i]+" ";
+        }
+        System.out.println("The reverse order is : "+result.trim());
+
+    }
+}
+/*
+- Pass Testing
+- 10 of 10
+ */
