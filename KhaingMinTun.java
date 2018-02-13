e4cfa7d0d209d471bfb588968615f9da1e62826e
BizLeapTechnology <bizleaptechnologyassignment@gmail.com>
2018-02-13 15:57:08 +0630

    KhaingMinTun.java

---
+++ KhaingMinTun.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class KhaingMinTun {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter FirstName MiddleName LastName: ");
+        String name = input.nextLine();
+        String[] words = name.split(" ");
+        for (int i = words.length - 1; i >= 0; i--) {
+            System.out.print(words[i]);
+        }
+
+    }
+}
+/*
+- Pass Test
+- 10 of 10
+ */
