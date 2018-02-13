9f9863061c8cd7446eda17471a2d78db55dd3931
BizLeapTechnology <bizleaptechnologyassignment@gmail.com>
2018-02-13 13:30:04 +0630

    MaPyaeSuMon

---
+++ MaPyaeSuMon.java
@@ -0,0 +1,20 @@
+public class MaPyaeSuMon {
+    public static void reverserNameFunction(String name)
+    {
+        StringBuilder ReverseName = new StringBuilder();
+        String[] words = name.split(" ");
+        for (int j = words.length-1; j >= 0; j-- ) {
+            ReverseName.append(words[j]).append(' ');
+        }
+        System.out.println("Reverse words: " + ReverseName);
+    }
+
+    public static void main(String[] args) {
+        reverserNameFunction("First Middle Last");
+    }
+}
+
+/*
+- Pass Testing
+- Does not follow Naming Convention
+ */
