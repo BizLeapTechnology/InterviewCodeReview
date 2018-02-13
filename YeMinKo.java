63891eb79e9cfc00b1e8d4317a70c39b7be51b1b
BizLeapTechnology <bizleaptechnologyassignment@gmail.com>
2018-02-13 14:20:44 +0630

    YeMinKo.java

---
+++ YeMinKo.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class YeMinKo {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.print("Enter the sentense: ");
+        String sentense=input.nextLine();
+        String[] words=sentense.split(" ");
+        for(int i=words.length-1;i>=0;i--){
+            System.out.print(words[i]+" ");
+        }
+    }
+}
+/*
+- Pass Testing
+- Follow naming convention
+- 10 of 10
+ */
