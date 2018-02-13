4e2b1d71792e9a0962b27b601ed75ebd08f4bb39
BizLeapTechnology <bizleaptechnologyassignment@gmail.com>
2018-02-13 13:25:40 +0630

    LwinLwinMar.java

---
+++ LwinLwinMar.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class LwinLwinMar {
+    public static void main(String[] args) {
+        StringBuffer sbf = new StringBuffer("First Middle Last");
+        System.out.println(sbf.reverse());
+    }
+}
+
+/*
+- Fail Testing
+- 0 of 10
+ */
