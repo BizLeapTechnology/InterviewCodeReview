a56176138fcaf64e9561af35e6f12c8a34702db6
BizLeapTechnology <bizleaptechnologyassignment@gmail.com>
2018-02-14 14:44:23 +0630

    ZawThanHtike.java

---
+++ ZawThanHtike.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ZawThanHtike {
+    public static void main(String[] args) {
+        String f,m,l;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter your name");
+        f= in.next();
+        m=in.next();
+        l=in.next();
+        System.out.println("Last Name:" + l +" Middle Name:"+ m +" First Name:"+ f);
+    }
+}
+/*
+- Pass Testing
+- 2nd Part Failed
+- 6 of 10
+ */
