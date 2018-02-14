84c14f5e623baa6f84797024c4c19a1d2399094d
BizLeapTechnology <bizleaptechnologyassignment@gmail.com>
2018-02-14 11:34:17 +0630

    AyeAyeKhine.java

---
+++ AyeAyeKhine.java
@@ -0,0 +1,13 @@
+public class AyeAyeKhine {
+    public static void main(String[] args) {
+        String input1 = "AyeAyeKhine";
+        StringBuilder input2 = new StringBuilder();
+        input2.append(input1);
+        input2 = input2.reverse();
+        System.out.print(input2);
+    }
+}
+/*
+- Fail Testing
+- 0 of 10
+ */
